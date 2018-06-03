41d4cac2fb630c536910c9db63a83206da22ba32
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-06-04 00:07:49 +0200

    Fixed tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,9 @@
+import * as path from 'path'
 import pluginTester from 'babel-plugin-tester'
 import macrosPlugin from 'babel-plugin-macros'
+import { addAlias } from 'module-alias'
+
+addAlias('data-uri.macro', path.join(__dirname, '..'))
 
 pluginTester({
 	title: 'data-uri.macro',
