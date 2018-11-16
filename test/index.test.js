78c892bb4fd5df992c26bb77e587ab421c608f12
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-11-16 11:19:18 +0100

    Use npm-self-link

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,9 @@
 import * as path from 'path'
 import pluginTester from 'babel-plugin-tester'
 import macrosPlugin from 'babel-plugin-macros'
-import { addAlias } from 'module-alias'
-
-addAlias('data-uri.macro', path.join(__dirname, '..'))
 
 pluginTester({
-	title: 'data-uri.macro',
-	plugin: macrosPlugin,
-	fixtures: `${__dirname}/fixtures`
+  title: 'data-uri.macro',
+  plugin: macrosPlugin,
+  fixtures: `${__dirname}/fixtures`,
 })
